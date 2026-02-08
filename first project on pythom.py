8871c632687e4989ef43e0e0a8977383b12bc058
laurcriminal842-alt <laurcriminal842@gmail.com>
2026-02-08 21:04:00 +0200

    Include time.sleep to pause after output
    Added a delay of 10 seconds after printing the result.

--- first project on pythom.py
+++ first project on pythom.py
@@ -2,8 +2,10 @@ firstnumber = int(input("Enter first number: "))
 secondnumber = int(input("Enter second number: "))
 fourthnumber = int(input("Enter fourth number: "))
 fifthnumber = int(input("Enter fifth number: "))
-
+import time
 
 mathlogic = firstnumber + secondnumber + fourthnumber + fifthnumber
 
 print(mathlogic)
+
+time.sleep(10)
